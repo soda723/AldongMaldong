4bc899616aa48abc0af537b71b65477abf7cc5b3
Jiwon <26162616@naver.com>
2021-11-01 15:15:06 +0900

    init

---
+++ AMproject/mainapp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path 
+from . import views
+
+app_name = 'mainapp'
+urlpatterns = [
+    path('', views.main, name='main'), #main
+]
---
+++ AMproject/mainapp/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render, redirect
+
+# Create your views here.
+
+def main(request):
+    return render(request, 'mainapp/main.html')
