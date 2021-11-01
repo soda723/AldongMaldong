0ec490b17a55a7a19bd1ea3965dec8c7beaca032
Jiwon <26162616@naver.com>
2021-11-01 15:36:33 +0900

    base file add

--- AMproject/AMproject/settings.py
+++ AMproject/AMproject/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'AMproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# 베이스 템플릿을 만들때 필요
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
