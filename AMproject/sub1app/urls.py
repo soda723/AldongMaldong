3e307f0c4f6e3cfbf043e4ecdbbef0ab04982ba6
Jiwon <26162616@naver.com>
2021-11-14 22:24:42 +0900

    sub1 centerinfo

--- AMproject/sub1app/urls.py
+++ AMproject/sub1app/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'sub1app'
 urlpatterns = [
     path('', views.searchRC, name='searchRC'),
+    path('center_info/<int:obj_pk>', views.center_info, name='center_info'),
 ]
--- AMproject/sub1app/views.py
+++ AMproject/sub1app/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from .models import RescueCenter
+from django.core.paginator import Paginator
 # Create your views here.
 
 def searchRC(request):
 
     context = {}
+    page = request.GET.get('page', 1) #페이지 디폴트 값1
 
     contents = RescueCenter.objects.all()
     if request.method == "POST":
@@ -30,7 +32,18 @@ def searchRC(request):
 
     else :
         #전체 리스트 보여주기
-        context['results'] = contents
+        paginator = Paginator(contents, 10)#페이지 당 10개씩
+        page_obj = paginator.get_page(page)
+        context['results'] = page_obj
         context['regions'] = ["전체"]
         return render(request, 'sub1app/searchRc.html', context)
+
+
+def center_info(request, obj_pk):
+    context={}
+
+    contents = RescueCenter.objects.get(pk=obj_pk)
+    context['data'] = contents
+
+    return render(request, 'sub1app/center_info.html', context)
 
