b368bfa900458b4adc36414f40a9bc6a97b08901
Jiwon <26162616@naver.com>
2021-12-02 17:42:36 +0900

    yolo_object_detection

--- AMproject/mainapp/views.py
+++ AMproject/mainapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 # from django.views.decorators.http import require_GET, require_POST
-
+from .models import box_image
+from yolo_object_detection.yolo_detection import result_str 
 
 # Create your views here.
 
@@ -12,14 +13,28 @@ def go_search(request):
 
 # @require_POST
 def search_result(request):
+    context={}
     
     if request.method == 'POST':
         if 'image' in request.FILES:
-            pass
+            imagee=request.FILES['image']
+            new_image=box_image(image = imagee)
+            new_image.save()
+            fname = imagee.name
+            file_path = f"./media/userimg/{fname}"
+            context['file_path'] = file_path
+
+            #결과값 받아오기
+            what = result_str(file_path)
+            context['what'] = what
+            #사용후 파일 지우기
+            #new_image.delete()
+            return render (request, 'mainapp/search_result.html', context)
+
         else:
             redirect('mainapp:go_search')
 
-        return render (request, 'mainapp/search_result.html')
+        
     else :
         #get 요청 -> main 페이지로
         return redirect('mainapp:main')
---
+++ AMproject/yolo_object_detection/yolo_detection.py
@@ -0,0 +1,55 @@
+import cv2
+import numpy as np
+print(cv2.__version__)
+
+def result_str(imagepath):
+
+    path_w = ".\yolo_object_detection\yolov3.weights"
+    path_cfg = ".\yolo_object_detection\yolov3.cfg"
+    path_name = ".\yolo_object_detection\coco.names"
+
+    #yolo 로드
+    net = cv2.dnn.readNet(path_w, path_cfg)
+    classes = []
+    with open(path_name, "r") as f:
+        classes = [line.strip() for line in f.readlines()]
+    layer_names = net.getLayerNames()
+    output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+    colors = np.random.uniform(0, 255, size=(len(classes), 3))
+
+    # 이미지 가져오기
+    img = cv2.imread(imagepath)
+    img = cv2.resize(img, None, fx=0.4, fy=0.4)
+    height, width, channels = img.shape
+
+    # Detecting objects
+    blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+    net.setInput(blob)
+    outs = net.forward(output_layers)
+
+    # 정보를 화면에 표시
+    class_ids = []
+    confidences = []
+    boxes = []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                # Object detected
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+                # 좌표
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+
+    #노이즈 제거???
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+
+    return classes[class_ids[0]]
